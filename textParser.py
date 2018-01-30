bd71a013c56f9b63355cc858c9ebcba5e7dea75b
chapman3 <chapman3@uoregon.edu>
2018-01-30 15:50:40 -0800

    Implemented text parser, works with given test case, further testing needed

--- textParser.py
+++ textParser.py
@@ -1,17 +1,32 @@
+import re
+import string
+import sys
+import collections
 
 def generateDict(text):
 	'''
 	Given a body of text (HTML or otherwise), returns an ordered dictionary of pairs in the format [word:num_occurences]
 	'''
 	#strip html tags
+	no_html = re.sub('<.*?>', ' ', text)
 	#strip punctuation
+	no_punc = no_html
+	no_punc = no_punc.replace("-"," ")
+	for p in string.punctuation:
+		no_punc = no_punc.replace(p,"")
 	#split text into words
+	word_array = no_punc.lower().split()
 	#create dict
-	words = Collections.OrderedDict()
+	words = collections.OrderedDict()
+	for word in word_array:
+		if word in words.keys():
+			words[word] += 1
+		else:
+			words[word] = 1
 	return words
 
 def test():
-	text = ""
+	text = "<p>These are words!</p><ul><li>These-These These are list items.</li><li>And then, is this, another.. list item??</li></ul>And more words, and some symbols -> !@#$%^&*()"
 	words = generateDict(text)
 	for k,v in words.items():
 		print k,v
